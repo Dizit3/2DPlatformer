f7beb6afb00eac016d55c7cd0606191ba5be95e4
Dizit3 <dizit3@gmail.com>
2023-01-06 03:44:53 +0200

    Animation

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -9,43 +9,62 @@ public class Hero : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator anim;
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
-
+        anim = GetComponent<Animator>();
     }
 
 
     private void Run()
     {
+        if (isGrounded) State = States.run;
+
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
 
-        sprite.flipX = dir.x < 0.0f; 
+        sprite.flipX = dir.x < 0.0f;
     }
 
 
     private void Update()
     {
+
+        if (isGrounded) State = States.idle;
+        else State = States.jump;
+
+
         if (Input.GetButton("Horizontal"))
             Run();
 
         if (Input.GetButtonDown("Jump") && isGrounded)
-        Jump();
+            Jump();
     }
 
     private void Jump()
     {
-
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        isGrounded = true;
+         isGrounded = true;
+
     }
+
 
     private void OnCollisionExit2D(Collision2D collision)
     {
@@ -53,3 +72,10 @@ public class Hero : MonoBehaviour
     }
 }
 
+
+public enum States
+{
+    idle,
+    run,
+    jump
+}
