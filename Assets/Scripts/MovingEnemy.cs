ea7bf4c692474864d74545820327a68a6e18d6d9
Dizit3 <dizit3@gmail.com>
2023-01-09 20:48:08 +0200

    AddMovementBehaviourToEnemy

--- Assets/Scripts/MovingEnemy.cs
+++ Assets/Scripts/MovingEnemy.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
+using UnityEngineInternal;
 
 public class MovingEnemy : Entity
 {
     private int lives = 3;
 
+    private Vector3 direction = Vector3.right;
+    //private float speed = 3.5f;
+
+    private SpriteRenderer sprite;
+
+    public Transform triangle;
+    private Ray2D myRay;
+    [SerializeField] private float rayDist;
+
+
+    private void Update()
+    {
+
+        Move();
+    }
 
 
 
+    private void Move()
+    {
+
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, Time.deltaTime);
+
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject == Hero.Instance.gameObject)
@@ -14,8 +38,30 @@ public class MovingEnemy : Entity
             Hero.onTouch.Invoke();
             GetDamage();
         }
-    }
+        if (collision.gameObject.tag == "Platform")
+        {
 
+            var hit = Physics2D.Raycast(new Vector2(triangle.position.x, triangle.position.y), Vector2.right, rayDist);
+
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+                Debug.DrawRay(new Vector2(triangle.position.x, triangle.position.y), new Vector3(rayDist,0), Color.black, 5f);
+
+                if (direction == Vector3.right)
+                {
+                    direction = Vector3.left;
+                }
+                else
+                {
+                    direction = Vector3.right;
+                }
+            }
+            
+
+        }
+
+    }
 
 
     public override void GetDamage()
